c82e36478d397f3af15d37eac9b1ab7559895f7e
Elineu <elineutomas4@gmail.com>
2025-07-17 23:14:01 +0100

    Add start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz/gradient_container.dart';
-
+import 'package:flutter_quiz/start_screen.dart';
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Scaffold(body: StartScreen(),),),);
 }
