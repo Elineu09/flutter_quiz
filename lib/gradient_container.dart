e6442a75cc2fe90b5bd0404dfe784caadacd1f40
Elineu <elineutomas4@gmail.com>
2025-07-17 22:38:53 +0100

    Add home screen

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/button.dart';
+
+const List<Color> colors = [
+  Color.fromARGB(255, 97, 25, 221),
+  Color.fromARGB(255, 36, 3, 92),
+];
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -8,22 +14,35 @@ class GradientContainer extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 97, 25, 221),
-            Color.fromARGB(255, 36, 3, 92),
-          ],
+          colors: colors,
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: Center(
-        child: Text(
-          style: TextStyle(
-            color: Colors.black45,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-          "Learn Flutter the fun way!!",
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 250,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              style: TextStyle(
+                color: Color.fromARGB(115, 211, 204, 204),
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+              'Learn Flutter the fun way!',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Button(),
+          ],
         ),
       ),
     );
