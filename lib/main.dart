2478b2970e303c565a6769aed43a030408069545
Elineu <elineutomas4@gmail.com>
2025-07-17 23:51:19 +0100

    Update start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/start_screen.dart';
+
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: StartScreen(),),),);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 161),
+                Color.fromARGB(255, 107, 17, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
