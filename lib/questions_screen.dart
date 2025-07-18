a2f8cc750be506fce46e6753863db7cea9e95330
Elineu <elineutomas4@gmail.com>
2025-07-18 21:55:17 +0100

    Add questions dynamically

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/answer_button.dart';
+import 'package:flutter_quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,10 +12,32 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  final currentQuestion = questions[0];
+
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Flutter Quiz!'),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          for (int i = 0; i < 4; i++)
+            AnswerButton(
+              answerText: currentQuestion.answers[i],
+              onTap: () {},
+            ),
+        ],
+      ),
     );
   }
 }
